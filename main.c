9271a4b030021b82bef3073e7f03bea2f1591f4b
kudo <kudo@techmatrix.co.jp>
2022-06-21 15:23:09 +0900

    MISRAの違反修正

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ uint16_t sub ( bool_t b, uint16_t *p );
 
 void sample ( void )
 {
-    //uint16_t ret;
+    uint16_t ret;
     uint16_t arg;
-    sub ( false, &arg );
-    if ( arg == 3 )
+    ret = sub ( false, &arg );
+    if ( (arg == 3U) && (ret == 0U) )
     {
     }
     return ;
